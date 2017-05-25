4bd6da249451d7f8c6be52b45c86b330271646f3
Kathleen Yang <kathleen.yang@users.noreply.github.com>
2017-05-24 23:20:47 -0500

    update 9

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,5 @@
 class LikesController < ApplicationController
+    before_action :authenticate_user!
   def index
     @likes = current_user.liked_photos
 
@@ -26,7 +27,7 @@ class LikesController < ApplicationController
     save_status = @like.save
 
     if save_status == true
-      redirect_to("/likes/#{@like.id}", :notice => "Like created successfully.")
+      redirect_to("/photos", :notice => "Like created successfully.")
     else
       render("likes/new.html.erb")
     end
